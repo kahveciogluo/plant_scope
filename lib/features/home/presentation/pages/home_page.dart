734fde97bf19b98a89510ee552b6aab948d5368b
Özlem Kahvecioğlu <80224415+kahveciogluo@users.noreply.github.com>
2025-11-10 11:49:14 +0300

    add pull to refresh

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -19,51 +19,67 @@ class HomePage extends StatelessWidget {
         appBar: const HomeAppBar(),
         body: BlocBuilder<HomeBloc, HomeState>(
           builder: (context, state) {
-            return Column(
-              children: [
-                /// Question horizontal list
-                SizedBox(
-                  height: context.width * 0.7 * 0.6,
-                  child: state.isLoadingQuestions
-                      ? const QuestionListLoading()
-                      : state.questions.isEmpty
-                      ? const Center(child: Text('No questions available'))
-                      : ListView.separated(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          scrollDirection: Axis.horizontal,
-                          itemCount: state.questions.length,
-                          separatorBuilder: (context, index) =>
-                              const SizedBox(width: 10),
-                          itemBuilder: (context, index) {
-                            final question = state.questions[index];
-                            return QuestionCard(question: question);
-                          },
-                        ),
-                ).bottomPadding(16),
-
-                /// Category list
-                Expanded(
-                  child: state.isLoadingCategories
-                      ? const CategoryListLoading()
-                      : state.categories.isEmpty
-                      ? const Center(child: Text('No categories available'))
-                      : GridView.builder(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                childAspectRatio: 1,
-                                crossAxisSpacing: 16,
-                                mainAxisSpacing: 16,
+            return RefreshIndicator(
+              onRefresh: () async {
+                context.read<HomeBloc>().add(GetHomeDataEvent());
+                // Wait a bit for the refresh to start
+                await Future.delayed(const Duration(milliseconds: 300));
+              },
+              child: SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Column(
+                  children: [
+                    /// Question horizontal list
+                    SizedBox(
+                      height: context.width * 0.7 * 0.6,
+                      child: state.isLoadingQuestions
+                          ? const QuestionListLoading()
+                          : state.questions.isEmpty
+                          ? const Center(child: Text('No questions available'))
+                          : ListView.separated(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
                               ),
-                          itemCount: state.categories.length,
-                          itemBuilder: (context, index) {
-                            final category = state.categories[index];
-                            return CategoryCard(category: category);
-                          },
-                        ),
+                              scrollDirection: Axis.horizontal,
+                              itemCount: state.questions.length,
+                              separatorBuilder: (context, index) =>
+                                  const SizedBox(width: 10),
+                              itemBuilder: (context, index) {
+                                final question = state.questions[index];
+                                return QuestionCard(question: question);
+                              },
+                            ),
+                    ).bottomPadding(16),
+
+                    /// Category list
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      child: state.isLoadingCategories
+                          ? const CategoryListLoading()
+                          : state.categories.isEmpty
+                          ? const Center(child: Text('No categories available'))
+                          : GridView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                              ),
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    childAspectRatio: 1,
+                                    crossAxisSpacing: 16,
+                                    mainAxisSpacing: 16,
+                                  ),
+                              itemCount: state.categories.length,
+                              itemBuilder: (context, index) {
+                                final category = state.categories[index];
+                                return CategoryCard(category: category);
+                              },
+                            ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             );
           },
         ),
